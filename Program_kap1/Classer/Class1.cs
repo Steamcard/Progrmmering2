6b3d67caecc2075c9fa40f5b95cbc274e0de4a6f
Kasper Falk <kasfal0128@skynet.halmstad.se>
2019-09-16 09:43:11 +0200

    czz

---
+++ Program_kap1/Classer/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classer
+{
+    class Class1
+    {
+        public string color;
+        public string brand;
+        public int year;
+    }
+
+    public Class1()
+    {
+
+    }
+}
---
+++ Program_kap1/Resebokning/bokning.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resebokning
+{
+    class Bokning
+    {
+        //Medelm
+
+        string namn = "";
+        string destination = "";
+        int antalDagar = 0;
+    }
+
+    private Resa(string n, string d, int a)
+    {
+        this.namn = n;
+        this.destination = d;
+        this.antalDagar = a;
+    }
+
+    private void ResaUtdata(string n, string d, int a)
+    {
+        return n + " " + d + " " + a + " dagar";
+    }
+}
