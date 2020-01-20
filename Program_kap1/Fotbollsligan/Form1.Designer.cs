502414bdeaa38625880e9185ca63b4b54eb32fb3
Kasper Falk <kasfal0128@skynet.halmstad.se>
2020-01-20 09:44:09 +0100

    -

---
+++ Program_kap1/Fotbollsligan/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Fotbollsligan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nyMatch = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nyBortalag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.molHemma = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.molBorta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnMolRik = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nyMatch,
+            this.nyBortalag,
+            this.molHemma,
+            this.molBorta});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(644, 285);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // nyMatch
+            // 
+            this.nyMatch.HeaderText = "Match";
+            this.nyMatch.Name = "nyMatch";
+            this.nyMatch.Width = 200;
+            // 
+            // nyBortalag
+            // 
+            this.nyBortalag.HeaderText = "Bortalag";
+            this.nyBortalag.Name = "nyBortalag";
+            this.nyBortalag.Width = 200;
+            // 
+            // molHemma
+            // 
+            this.molHemma.HeaderText = "Mål (H)";
+            this.molHemma.Name = "molHemma";
+            // 
+            // molBorta
+            // 
+            this.molBorta.HeaderText = "Mål (B)";
+            this.molBorta.Name = "molBorta";
+            // 
+            // btnMolRik
+            // 
+            this.btnMolRik.Location = new System.Drawing.Point(12, 308);
+            this.btnMolRik.Name = "btnMolRik";
+            this.btnMolRik.Size = new System.Drawing.Size(92, 23);
+            this.btnMolRik.TabIndex = 1;
+            this.btnMolRik.Text = "Målrikast";
+            this.btnMolRik.UseVisualStyleBackColor = true;
+            this.btnMolRik.Click += new System.EventHandler(this.btnMolRik_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(521, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 343);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnMolRik);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nyMatch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nyBortalag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn molHemma;
+        private System.Windows.Forms.DataGridViewTextBoxColumn molBorta;
+        private System.Windows.Forms.Button btnMolRik;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Program_kap1/Fotbollsligan/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fotbollsligan
+{
+    public partial class Form1 : Form
+    {
+        List<Match> matcher = new List<Match>;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMolRik_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string hemmalag = "";
+            string bortalag = "";
+            int antalMål = 0;
+
+        }
+    }
+
+    public class Match
+    {
+
+    }
+}
